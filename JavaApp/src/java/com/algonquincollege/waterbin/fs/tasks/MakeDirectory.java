07cc53ac241865967d3595db64c7b57415408608
stjohnd <devon.t.st.john@gmail.com>
2018-04-13 19:50:13 -0400

    Commit of Comments

--- JavaApp/src/java/com/algonquincollege/waterbin/fs/tasks/MakeDirectory.java
+++ JavaApp/src/java/com/algonquincollege/waterbin/fs/tasks/MakeDirectory.java
@@ -6,14 +6,17 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
- *
+ * Class representing an asyncronous creation of a directory
  * @author Devon St. John
  */
 public class MakeDirectory extends FileSystemTask{
 
     private final String path;
     
-    
+    /**
+     * Initial Constructor for the MakeDirectory task
+     * @param the path to create the directory at
+     */
     public MakeDirectory(String path){
         this.path = path;
         
@@ -21,6 +24,9 @@ public class MakeDirectory extends FileSystemTask{
         System.out.println("Make Directory Task: Launched to Aggregator");
     }
     
+    /**
+     * Handles all the logic of the MakeDirectory operation, can be called as part of an executor service
+     */
     @Override
     public void run() {
         System.out.println("Make Directory Task: " + path + " | Going Live");
@@ -40,6 +46,10 @@ public class MakeDirectory extends FileSystemTask{
         
     }
     
+    /**
+     * Get whether or not the operation was successful
+     * @return whether or not the directory was created
+     */
     @Override
     public boolean getSuccess(){
         return Files.exists(Paths.get(root, path));
