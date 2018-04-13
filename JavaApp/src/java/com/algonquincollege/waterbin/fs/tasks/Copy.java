2e3debcf3499e34687bae7f563b8b7be4eed7e40
stjohnd <devon.t.st.john@gmail.com>
2018-04-13 19:41:07 -0400

    Commit of Comments

--- JavaApp/src/java/com/algonquincollege/waterbin/fs/tasks/Copy.java
+++ JavaApp/src/java/com/algonquincollege/waterbin/fs/tasks/Copy.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.algonquincollege.waterbin.fs.tasks;
 
 import com.algonquincollege.javaApp.database.DBConnection;
@@ -11,14 +6,19 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
- *
- * @author Devon St John
+ * Class representing a basic asyncronous copy file task
+ * @author Devon St. John
  */
 public class Copy extends FileSystemTask {
     
     private final String source;
     private final String dest;
     
+    /**
+     * Initial Constructor for the copy task
+     * @param the file to copy from
+     * @param the file to copy to
+     */
     public Copy(String source, String dest){
         this.source = source;
         this.dest = dest;
@@ -28,6 +28,9 @@ public class Copy extends FileSystemTask {
         System.out.println("Move Task: Launched to Aggregator");
     }
     
+    /**
+     * Handles all the logic of the move operation, can be called as part of an executor service
+     */
     @Override
     public void run() {
         System.out.println("Copy Task: " + source + " To " + dest + " | Going Live");
@@ -51,6 +54,10 @@ public class Copy extends FileSystemTask {
         System.out.println("Copy Task: Completed");
     }
    
+    /**
+     * Get whether or not the operation was successful
+     * @return whether or not the operation was successful
+     */
     @Override
     public boolean getSuccess(){
         return Files.exists(Paths.get(root, dest));
