8c5efcabc54438a69512626b46a0cf3e4ac54a71
stjohnd <devon.t.st.john@gmail.com>
2018-04-13 20:07:23 -0400

    Commit of Comments

--- JavaApp/src/java/com/algonquincollege/waterbin/fs/fsAggregator/FSAggregator.java
+++ JavaApp/src/java/com/algonquincollege/waterbin/fs/fsAggregator/FSAggregator.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.algonquincollege.waterbin.fs.fsAggregator;
 
 import com.algonquincollege.waterbin.fs.tasks.FileSystemTask;
@@ -14,36 +9,55 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * This class serves as the framework for a multi-threaded executor pool for file system tasks
  *
  * @author Devon St. John
  */
 public class FSAggregator {
     
+    //The executor thread pool itself
     private final ExecutorService pool;
     
+    /**Constructor for the aggregator, initializes the thread pool*/
     public FSAggregator(){
+        //a fixed thread pool is used to prevent overloading the host
         pool = Executors.newFixedThreadPool(8);
         System.out.println("File System Aggregator: Started");
     }
     
+    /**
+     * Add a new file system task to the executor pool, if no threads are avaliable,
+     * the task will be queued, and ran when a thread becomes available. This is
+     * a blocking function
+     * 
+     * @param task the task to execute
+     * @return whether the task succeeded
+     */
     public boolean addTask(FileSystemTask task){
         System.out.println("File System Aggregator: New Task Submitted");
+        //Commit the task to the executor pool and get a listener for it
         Future<FileSystemTask> ourFuture = (Future<FileSystemTask>) pool.submit(task);
         
         try {
+            //Wait for the task to complete
             ourFuture.get();
+            //return the status of the task
             return task.getSuccess();
         } catch (InterruptedException | ExecutionException ex) {
             Logger.getLogger(FSAggregator.class.getName()).log(Level.SEVERE, "Task encountered abnormal conditions", ex);
         }
+        //if the task encountered an abnormal contition, it is assumed to have failed
         return false;
     }
     
+    /**
+     * Called to inform the executor that it must stop accepting tasks and finish
+     * all committed tasks.
+     */
     public void shutdown(){
         System.out.println("File System Aggregator: Shuting Down");
+        //Shutdown the executor pool to stop new tasks from being added
+        //Also finishes all tasks already submitted
         pool.shutdown();
     }
-    
-    
-    
 }
